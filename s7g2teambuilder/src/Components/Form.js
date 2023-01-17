fff48fdd3f98c41a0dad97604bb072255995375d
Onur-DEMR <119036165+Onur-DEMR@users.noreply.github.com>
2023-01-18 01:25:40 +0300

    Formda düzenlemeler yapıldı.

--- s7g2teambuilder/src/Components/Form.js
+++ s7g2teambuilder/src/Components/Form.js
@@ -3,7 +3,22 @@ import React from "react";
 const Form = (props) => {
   return (
     <form>
-      <input />
+      <label>
+        Name
+        <input type="text" name="Name" />
+      </label>
+      <label>
+        E-mail
+        <input type="text" name="E-mail" />
+      </label>
+      <label>
+        Role
+        <input type="text" name="Role" />
+      </label>
+      <label>
+        Submit
+        <input type="submit" name="Submit" />
+      </label>
     </form>
   );
 };
